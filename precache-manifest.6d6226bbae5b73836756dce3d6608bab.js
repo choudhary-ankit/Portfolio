bf6e802baa62dd9ea281bf4998b3dd81bd86ada6
choudhary-ankit <ankit_chy@outlook.com>
2020-10-13 08:43:28 +0530

    Updates

---
+++ precache-manifest.6d6226bbae5b73836756dce3d6608bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "576bbc855552b9f0553e4e6422db75ad",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "385a8bca1c3d8bffc46a",
+    "url": "/Portfolio/static/css/main.6d2cc6c1.chunk.css"
+  },
+  {
+    "revision": "4478e4ecf75ec01f5e38",
+    "url": "/Portfolio/static/js/2.0e8e832d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.0e8e832d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385a8bca1c3d8bffc46a",
+    "url": "/Portfolio/static/js/main.1fa39e02.chunk.js"
+  },
+  {
+    "revision": "68b8423b8fee9004e793",
+    "url": "/Portfolio/static/js/runtime-main.3166be14.js"
+  }
+]);
